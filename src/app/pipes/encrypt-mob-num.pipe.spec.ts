2242041acfcefd6eb087f011ea6db23a7247e309
Vijay <vijayshende4@gmail.com>
2021-11-29 18:25:10 +0530

    deleted folders

--- src/app/pipes/encrypt-mob-num.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EncryptMobNumPipe } from './encrypt-mob-num.pipe';
-
-describe('EncryptMobNumPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EncryptMobNumPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/student/student.ts
+++
@@ -1,6 +0,0 @@
-export interface Student {
-  id: string,
-  rollNumber: number,
-  name: string,
-  imagePath: string
-}

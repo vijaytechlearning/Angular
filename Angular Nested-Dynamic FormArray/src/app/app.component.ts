868d84bff6dccf6a9addcc9529a2b0d5b2c21e42
Vijay <vijayshende4@gmail.com>
2022-02-05 23:19:21 +0530

    Angular Nested / Dynamic FormArray

---
+++ Angular Nested-Dynamic FormArray/src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'form-array';
+  categoryForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.categoryForm = this.fb.group({
+      categories: this.fb.array([])
+    });
+  }
+
+  categories(): FormArray {
+    return this.categoryForm.get('categories') as FormArray;
+  }
+
+  newCategory(): FormGroup {
+    return this.fb.group({
+      categoryID: '',
+      categoryName: '',
+      sections: this.fb.array([])
+    });
+  }
+
+  addCategory() {
+    this.categories().push(this.newCategory());
+  }
+
+  removeCategory(catIndex: number) {
+    this.categories().removeAt(catIndex);
+  }
+
+  sections(catIndex: number): FormArray {
+    return this.categories()
+      .at(catIndex)
+      .get('sections') as FormArray;
+  }
+
+  newSection(): FormGroup {
+    return this.fb.group({
+      sectionID: '',
+      sectionName: '',
+      subSections: this.fb.array([])
+    });
+  }
+
+  addSection(catIndex: number) {
+    this.sections(catIndex).push(this.newSection());
+  }
+
+  removeSection(catIndex: number, sectionIndex: number) {
+    this.sections(catIndex).removeAt(sectionIndex);
+  }
+
+  subSections(catIndex: number, sectionIndex: number): FormArray {
+    let skills = this.categories()
+      .at(catIndex)
+      .get('sections') as FormArray;
+    let subSkills = skills.at(sectionIndex).get('subSections') as FormArray;
+    return subSkills;
+  }
+
+  subSection(): FormGroup {
+    return this.fb.group({
+      subSectionName: ''
+    });
+  }
+
+  addSubSection(catIndex: number, sectionIndex: number){
+    this.subSections(catIndex, sectionIndex).push(this.subSection());
+  }
+
+  removeSubSection(catIndex: number, sectionIndex: number, subSectionIndex: number) {
+    this.subSections(catIndex, sectionIndex).removeAt(subSectionIndex);
+  }
+
+  onSubmit() {
+    // console.log(this.categoryForm.value);
+  }
+}
